f1ab51a6614e0376fe17671f988fbe5caf18e7a8
gracekarinn <gracekarina12@gmail.com>
2024-10-17 12:05:11 +0700

    feat: Update restaurant_list view to include debug information

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Restaurant
 
-# Create your views here.
+def restaurant_list(request):
+    restaurants = Restaurant.objects.all()
+    
+    debug_info = {
+        'database_connection': 'Connected',
+        'model_fields': [field.name for field in Restaurant._meta.get_fields()],
+        'test_restaurant': Restaurant.objects.first(),
+        'restaurant_count': Restaurant.objects.count(),
+    }
+    
+    context = {
+        'restaurants': restaurants,
+        'debug_info': debug_info,
+    }
+    
+    return render(request, 'main.html', context)
