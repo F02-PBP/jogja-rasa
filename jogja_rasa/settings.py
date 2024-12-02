05a13e048f297b77d631f3682bee9825efbdf3ff
gracekarinn <gracekarina12@gmail.com>
2024-12-02 23:34:52 +0700

    fix: correct CORS middleware configuration in settings

--- jogja_rasa/settings.py
+++ jogja_rasa/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions', 
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders',
     'main',
     'restaurants',
     'reservation',
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'jogja_rasa.urls'
