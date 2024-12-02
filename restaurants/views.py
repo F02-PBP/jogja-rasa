e0fcde6e3aab28ab5d296b02bb1595d9d91df29f
gracekarinn <gracekarina12@gmail.com>
2024-12-02 23:35:03 +0700

    feat: add get_restaurants view to retrieve all restaurant data as JSON

--- restaurants/views.py
+++ restaurants/views.py
@@ -8,3 +8,15 @@ def restaurant_list(request):
         'restaurants': restaurants,
     }
     return render(request, 'main.html', context)
+
+def get_restaurants(request):
+    restaurants = Restaurant.objects.all()
+    data = [{
+        'id': str(restaurant.id),
+        'name': restaurant.name,
+        'longitude': restaurant.longitude,
+        'latitude': restaurant.latitude,
+        'description': restaurant.description,
+        'location': restaurant.get_location()
+    } for restaurant in restaurants]
+    return JsonResponse({'restaurants': data})
