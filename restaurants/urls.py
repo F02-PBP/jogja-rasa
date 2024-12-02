e260e5804e063123089c236c49d3d8b1bf96b8f6
gracekarinn <gracekarina12@gmail.com>
2024-12-02 23:35:00 +0700

    feat: add new URL path for retrieving restaurants

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from views import restaurant_list
+from .views import restaurant_list, get_restaurants
 
 urlpatterns = [
     path('', restaurant_list, name='restaurant_list'),
+    path('get_restaurants/', get_restaurants, name='get_restaurants'),
 ]
